8d347ee88370b66e6ca469a0cbc52ceed1323703
GeoffWebster <77614804+GeoffWebster@users.noreply.github.com>
2025-06-20 16:39:53 +0100

    Add files via upload

--- Muses72323.h
+++ Muses72323.h
@@ -20,62 +20,97 @@
   IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-/*
-  Amended 2024 Geoff Webster
-
-  1. Clock speed amended to align with data sheet limits (1MHz) + safety margin. Clock speed now set to 800KHz
-  2. setGain routine now impemented so that gain may be set between 0dB (default gain) and +21dB in 3dB steps.
-*/
 
 #ifndef INCLUDED_MUSES_72323
 #define INCLUDED_MUSES_72323
 
 #include <Arduino.h>
+#include "SPI.h"
 
-class Muses72323 {
-  public:
-    // contextual data types
-    typedef uint8_t pin_t;
-    typedef uint16_t data_t;
-    typedef int volume_t;
-    typedef uint16_t address_t;
+//  ERROR CODES
+#define Muses72323_OK                       0x00
+//#define Muses72323_PIN_ERROR                0x81
+//#define Muses72323_SPI_ERROR                0x82
+#define Muses72323_VALUE_ERROR              0x83
+//#define Muses72323_PORT_ERROR               0x84
+//#define Muses72323_REGISTER_ERROR           0xFF
 
-    // specify a connection over an address using three output pins
-    Muses72323(address_t chip_address, pin_t latch);
+#ifndef __SPI_CLASS__
+  #if defined(ARDUINO_ARCH_RP2040)
+  #define __SPI_CLASS__   SPIClassRP2040
+  #else
+  #define __SPI_CLASS__   SPIClass
+  #endif
+#endif
+
+const uint32_t Muses72323_TYP_SPI_SPEED =  800000;
+const uint32_t Muses72323_MAX_SPI_SPEED = 1000000;
+
+class Muses72323
+{
+public:
+  // contextual data types
+  typedef uint8_t pin_t;
+  typedef uint16_t data_t;
+  typedef int volume_t;
+  typedef uint16_t address_t;
+
+  //  Custom SPI
+  Muses72323(uint8_t sclk, uint8_t miso, uint8_t mosi, uint8_t select, uint8_t chip_address = 0x00);
+  //  HARDWARE SPI
+  Muses72323(uint8_t select, __SPI_CLASS__ *spi);
+  Muses72323(uint8_t select, uint8_t chip_address = 0x00, __SPI_CLASS__ *spi = &SPI);
+  
+  // set the pins in their correct states
+  bool begin();
+  bool begin(uint8_t sclk, uint8_t miso, uint8_t mosi, uint8_t select);
+
+  // set the volume using the following formula:
+  // (-0.25 * volume) db
+  // audio level goes from -111.75 to 0.0 dB
+  // input goes from -447 to 0
+  uint8_t setVolume(volume_t left, volume_t right);
+
+  // this function sets the settings in the gain address
+  uint8_t setGain(data_t intgain);
+
+  void mute();
+  // must be set to false if no external clock is connected
+  void setExternalClock(bool enabled);
+
+  // enable or disable zero crossing
+  void setZeroCrossingOn(bool enabled);
+
+  // if set to true left and right will not be treated independently
+  // to set attenuation with linked channels just set the left channel
+  void setLinkChannels(bool enabled);
+
+private:
+  void transfer(address_t reg, data_t data);
+
+  //       access to low level registers (just make these two functions public).
+  //       USE WITH CARE !!!
+  bool     writeReg(uint8_t reg, uint8_t value);
+
+  uint8_t  _chip_address = 0;
+  uint8_t  _select  = 0;
+  uint8_t  _miso = 0;
+  uint8_t  _mosi  = 0;
+  uint8_t  _sclk   = 0;
+  uint8_t  _error   = Muses72323_OK;
+
+  bool     _hwSPI   = false;
+
+  //  1 MHz is maximum, 800K is a better clock divider.
+  uint32_t _SPIspeed = Muses72323_TYP_SPI_SPEED;
+  __SPI_CLASS__ * _mySPI;
+  SPISettings   _spi_settings;
  
-    // set the pins in their correct states
-    void begin();
+  address_t chip_address;
+  data_t states;
+  data_t gain;
 
-    // set the volume using the following formula:
-    // (-0.25 * volume) db
-    // audio level goes from -111.75 to 0.0 dB
-    // input goes from -447 to 0
-    void setVolume(volume_t left, volume_t right);
-
-    // set the gain using the following formula:
-    // (3 * ampgain) db
-    // audio gain goes from 0 to 21dB in 3dB steps
-    // input goes from 0 to 7
-    void setGain(data_t ampgain);
-
-    void mute();
-    // must be set to false if no external clock is connected
-    void setExternalClock(bool enabled);
-
-    // enable or disable zero crossing
-    void setZeroCrossingOn(bool enabled);
-
-    // if set to true left and right will not be treated independently
-    // to set attenuation with linked channels just set the left channel
-    void setLinkChannels(bool enabled);
-
-  private:
-    void transfer(address_t address, data_t data);
-
-    // for multiple chips on the same bus line
-    address_t chip_address;
-    data_t states ;
-    data_t gain ;
+  uint8_t  swSPI_transfer(uint8_t val);
 };
 
 #endif // INCLUDED_MUSES_72323
